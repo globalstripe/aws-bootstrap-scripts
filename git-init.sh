525732b1da9955f5eacf2ebfaf564993fcc3c0b4
Chris Clark <chris.clark@globalstripe.com>
2020-07-28 06:34:02 +0100

    Second commit

---
+++ git-init.sh
@@ -0,0 +1,6 @@
+echo "# aws-bootstrap-scripts" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:globalstripe/aws-bootstrap-scripts.git
+git push -u origin master
